30c1d1a6131f7eecf568bf6a42ad1858093fa65d
Nijanthandeveeikan <nijanthandeveeikan06@gmail.com>
2024-02-18 00:01:17 +0530

    sample program added

---
+++ src/test/java/Fblogin.java
@@ -0,0 +1,58 @@
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class Fblogin {
+	
+
+    //2. private web elements
+    //WebElement email = driver.findElement(By.id("email"));
+	
+	private WebElement varRef;
+	
+	
+	@FindAll ({
+	@FindBy(xpath = "//input[@id='email']"),
+	@FindBy(xpath ="//input[@name='email']"),
+	@FindBy(xpath = "//input[@data-testid='royal_email']']")
+})
+	@FindBy(xpath = "//input[@aria-label ='email']" )
+	private WebElement email;
+	
+	@FindBy(xpath = "//input[@aria-label ='Password']" )
+	private WebElement password;
+	
+	@FindBy(name = "login")
+	private WebElement loginBtn;
+
+
+	
+	
+	// 3. Getters to access webelements outside the class
+	
+	public WebElement getVarRef() {
+		return varRef;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public WebElement getLoginBtn() {
+		return loginBtn;
+	}
+
+	public WebElement getEmail() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+
+
+
+	}
+
+
